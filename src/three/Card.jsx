e0ef69c82bac291b29dc55c3857ba2f8017b26bc
Vishnu R <vishnu6364748848@gmail.com>
2026-09-17 14:11:27 +0530

    feat: compute texture aspect ratio using useMemo in Card component

--- src/three/Card.jsx
+++ src/three/Card.jsx
@@ -1,9 +1,15 @@
 import { useTexture } from '@react-three/drei'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 const Card = ({project}) => {
 
     const texture = useTexture(project.thumbnail);
+
+    useMemo(()=>{
+        const img = texture.image;
+        const aspect = img ? img.width / img.height : 1;
+        
+    })
 
   return (
     <>
